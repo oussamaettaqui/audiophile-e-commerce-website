b13368ed6cf32b0811f266f193618e63f91aaaa0
oettaqui <oettaqui@e1r11p2.1337.ma>
2024-05-12 23:56:56 +0100

    v1

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+
+// const productBanner = document.querySelector('.product-banner');
+
+// fetch('../data.json')
+//     .then(response => response.json())
+//     .then(data => {
+// 		for (let i = 0; i < data.length; i++) {
+// 			const product = data[i];
+// 			if (product.name === 'XX99 Mark II Headphones') {
+// 			  const imageUrl = product.image.desktop;
+// 			  console.log("Image URL: ", imageUrl);
+// 			  productBanner.src = `${imageUrl}`;
+// 			  break;
+// 			}
+// 		  }
+// 		// console.log("Fetched Data:", data)
+//         // const matchingProduct = data.find(product => product.name === 'XX99 Mark II Headphones');
+
+// 		// if (matchingProduct) {
+// 		// 	const imageUrl = matchingProduct.imageUrl;
+// 		// 	console.log("Image URL:", imageUrl);
+// 		// 	// ...
+// 		// }
+// 		//  else {
+//         //     console.warn('Product "XX99 Mark II" not found in data.json');
+//         //     // Handle case where product is not found (e.g., display a placeholder)
+//         // }
+//     })
+//     .catch(error => {
+//         console.error('Error fetching data:', error);
+//     });
